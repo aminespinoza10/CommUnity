25331867b16b96d06df9afe004ae623f8832832d
marcelaquiroga <m2q_83@hotmail.com>
2025-02-12 18:35:28 -0600

    Terraform file updated in Infraestructure

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name = "${var.prefix}-vecinos-rg"
+  name = "${var.prefix}-commUnity-rg"
   location = var.location
 }
 
 resource "azurerm_container_app_environment" "container_app_environment" {
-  name                           = "${var.prefix}-vecinos-env"
+  name                           = "${var.prefix}-commUnity-env"
   location                       = azurerm_resource_group.rg.location
   resource_group_name            = azurerm_resource_group.rg.name
   zone_redundancy_enabled        = false
@@ -13,4 +13,32 @@ resource "azurerm_container_app_environment" "container_app_environment" {
     name                  = "Consumption"
     workload_profile_type = "Consumption"
   }
+}
+
+resource "azurerm_servicebus_namespace" "servicebus_namespace" {
+  name                = "${var.prefix}-commUnity-servicebus-namespace"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "general_servicebus_queue" {
+  name         = "general_balance_queue"
+  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
+
+  partitioning_enabled = true
+}
+
+resource "azurerm_servicebus_queue" "year_servicebus_queue" {
+  name         = "year_balance_queue"
+  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
+
+  partitioning_enabled = true
+}
+
+resource "azurerm_servicebus_queue" "monthly_servicebus_queue" {
+  name         = "monthly_balance_queue"
+  namespace_id = azurerm_servicebus_namespace.servicebus_namespace.id
+
+  partitioning_enabled = true
 }
